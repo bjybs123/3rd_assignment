8d13c10741f5b0415d979c6eeb70f12eb70a6301
Jiyong Park <bjybs123@outlook.com>
2021-05-18 23:35:09 +0900

    erase dump memory leaks check

--- Assignment3_7/Assignment3_7/student_node.h
+++ Assignment3_7/Assignment3_7/student_node.h
@@ -4,14 +4,8 @@
 
 #include <iostream>
 #include <cstdlib>
-#include <crtdbg.h>
 
-#ifdef _DEBUG
-#ifndef DBG_NEW 
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
-#define new DBG_NEW 
-#endif 
-#endif // _DEBUG
+
 using namespace std;
 
 #ifdef _DEBUG
